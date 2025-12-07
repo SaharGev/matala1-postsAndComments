fe45b9f2a1128c9d3326fee8b886ec7fb2850c5a
SaharGev <sahargevm@gmail.com>
2025-12-07 22:30:40 +0200

    initial

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+app.use(express.json());
+
+const postRouter = require('./routes/postRoutes');
+app.use('/post', postRouter);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+module.exports = app;
