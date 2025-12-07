83d1133dffc63047c29c6163507ca860dad686a8
SaharGev <sahargevm@gmail.com>
2025-12-07 22:38:41 +0200

    Add GET all posts API

--- models/postModel.js
+++ models/postModel.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
 
-
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    senderId: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now   
+    }
+}); 
 
 module.exports = mongoose.model('post', postSchema);
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const postController = require('../controllers/postController');
+const postController = require('../controllers/postControllers');
 
+router.get('/', postController.getAllPosts);
 
 module.exports = router;
